f7caf4bb2ec098e048d18da31f2dd7ba40249032
jun.wang <jun.wang@sheng-sen.com>
2023-08-23 17:46:51 +0800

    調整1

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,63 +1,3 @@
-/***
-    * 监听滚动事件，判断滚动方向
-    * 
- ***/
-// var Before_scollH = 0;
-// let show = false;
-
-// function disableScroll() {
-//   // 获取当前滚动位置
-//   var scrollTop =
-//     window.pageYOffset || document.documentElement.scrollTop;
-//   // var scrollLeft =
-//   //   window.pageXOffset || document.documentElement.scrollLeft;
-
-//   window.onscroll = function (e) {
-//     let scrollY = this.scrollY;
-//     // window.scrollTo(0, scrollTop);
-//     console.log(scrollTop, scrollY);
-//   };
-// }
-
-// window.addEventListener("scroll", (e) => {
-//   var After_scollH = window.pageYOffset || document.documentElement.scrollTop;
-//   let avalibleHeight = document.querySelector(".home_box2").offsetHeight;
-
-//   let txt = document.querySelectorAll("#A2_txt span")
-
-//   var differH = After_scollH - Before_scollH;
-//   if (differH == 0) {
-//     return false;
-//   }
-//   var scollType = differH > 0 ? 'down' : 'up';
-
-//   console.log("type", Before_scollH, After_scollH, scollType);
-
-//   // if (scollType==='down') {
-//   //   if (After_scollH < avalibleHeight){
-
-//   //   }
-//   // }
-
-//   if (differH > 0 && After_scollH < avalibleHeight) {
-//     scrollToCmtList();
-//   } else if (differH < 0 && After_scollH < avalibleHeight) {
-//     window.scrollTo(0, 0);
-//     document.querySelector(".raindrop").classList.remove('show');
-//     show = false;
-//   }
-
-//   if (show === true) {
-
-//   }
-
-//   Before_scollH = After_scollH;
-// })
-
-// window.scrollTo(0, 0)
-// scrollToCmtList(getOffset(document.querySelector(".home_box4")).top)
-
-
 
 var deltaY = 0;
 var scollType = '';
@@ -76,16 +16,25 @@ document.addEventListener("wheel", function (e) {
     deltaY += e.deltaY;
     if (deltaY >= 300) {
       switch (step) {
+
         case 0:
           // document.querySelector(".raindrop").classList.add('show');
           step = 1;
           scrollToCmtList(getOffset(document.querySelector(".home_box2")).top);
-          document.querySelector(".rain_box").classList.add('show');
-          document.querySelector(".rain_box").classList.add('step4');
-          timeout = true;
-          setTimeout(() => {
-            timeout = false;
-          }, 2500);
+          const rainBox = document.querySelector(".rain_box");
+          rainBox.classList.add('show');
+          rainBox.classList.add('step4');
+
+          if (rainBox.getAttribute('class')) {
+            if (rainBox.className.indexOf('step4') > -1) {
+              timeout = true;
+              setTimeout(() => {
+                timeout = false;
+              }, 2500);
+            }
+          }
+
+
           break;
         case 1:
           // document.querySelectorAll("#A2_txt span")[0].classList.add('show')
@@ -108,6 +57,7 @@ document.addEventListener("wheel", function (e) {
         //   break;
         case 5:
           scrollToCmtList(getOffset(document.querySelector(".home_box3")).top);
+          document.querySelector(".home_box3 .txt").classList.add('on')
           step = 6;
           break;
         case 6:
@@ -194,9 +144,145 @@ function getOffset(el) {
     left: _x
   };
 }
-// window.addEventListener("keydown", function (e) {
-//   if (e.key == "ArrowDown" || e.key == "ArrowUp") {
-//     e.preventDefault();
-//   }
-// })
 
+
+var menu = false;
+function menuSwitch() {
+  menu = !menu;
+  var nav = document.getElementById('nav');
+  if (menu) {
+    nav.classList.add('on');
+  } else {
+
+    nav.classList.remove('on');
+  }
+}
+
+
+
+
+var home_box6Line = document.querySelectorAll(".home_box6 .content .box .line")
+var home_box6P = document.querySelectorAll(".home_box6 .content .box p")
+home_boxPosition();
+function home_boxPosition() {
+
+  for (let i = 0; i < home_box6P.length; i++) {
+    home_box6Line[i].style.height = home_box6P[i].offsetTop - 30 + "px";
+    if (i === 0) {
+      home_box6Line[i].style.left = home_box6P[i].offsetLeft + 30 + "px";
+    } else {
+      home_box6Line[i].style.left = home_box6P[i].offsetLeft + home_box6P[i].clientWidth - 30 + "px"
+    }
+  }
+}
+
+
+
+const carouselBox = document.querySelector('.carousel_box');
+let home_box7CarouselBox = 0;
+let home_box7CarouselNumber = 3;
+const data = [
+  { year: "2016", description: "2016" },
+  { year: "2017", description: "2017" },
+  { year: "2018", description: "建立3大標準程序" },
+  { year: "2019", description: "推動銀髮安逡計畫" },
+  { year: "2020", description: "首創地址編碼" },
+  { year: "2021", description: "建置大表與官網上線" },
+  { year: "2022", description: "2022" }
+];
+const carouselItem = [];
+
+// console.log(carouselBox.querySelectorAll('div'))
+
+
+function home_box7Carousel() {
+  let number = 0;
+  if (carouselBox.clientWidth > 1280) {
+    number = 4
+  } else if (carouselBox.clientWidth > 990) {
+    number = 3
+  } else {
+    number = 2
+  }
+
+  if (home_box7CarouselBox !== number) {
+    const removediv = carouselBox.querySelectorAll('div');
+    if (removediv) {
+      for (let i = 0; i < removediv.length; i++) {
+        removediv[i].remove();
+      }
+    }
+    home_box7CarouselBox = number;
+
+    for (let i = 0; i < home_box7CarouselBox; i++) {
+      carouselItem[i] = document.createElement('div');
+      const num = home_box7CarouselBox - 2;
+      if (i === num) {
+        carouselItem[i].innerHTML = `
+          <span>${data[home_box7CarouselNumber + i - num].year}</span>
+          <p>${data[home_box7CarouselNumber + i - num].description}</p>
+          <button class="left" onClick="home_box7CarouselSwitch(-1)"><</button>
+          <button class="right" onClick="home_box7CarouselSwitch(1)">></button>
+        `;
+        carouselItem[i].classList.add("active")
+      } else {
+        carouselItem[i].innerHTML = `
+          <span>${data[home_box7CarouselNumber + i - num].year}</span>
+          <p>${data[home_box7CarouselNumber + i - num].description}</p>
+        `;
+      }
+      carouselBox.appendChild(carouselItem[i]);
+    }
+  }
+}
+
+function home_box7CarouselSwitch(e) {
+  // console.log(e);
+  // home_box7CarouselNumber = (home_box7CarouselNumber + e !== 0) ?
+  const num = home_box7CarouselNumber + e;
+  if (num < 0) {
+    home_box7CarouselNumber = data.length
+  } else if (num > data.length) {
+    home_box7CarouselNumber = 0
+  } else {
+    home_box7CarouselNumber = num
+  }
+  console.log(home_box7CarouselNumber);
+  const removediv = carouselBox.querySelectorAll('div');
+  if (removediv) {
+    for (let i = 0; i < removediv.length; i++) {
+      removediv[i].remove();
+    }
+  }
+
+  for (let i = 0; i < home_box7CarouselBox; i++) {
+    carouselItem[i] = document.createElement('div');
+    const num = home_box7CarouselBox - 2;
+    if (i === num) {
+      carouselItem[i].innerHTML = `
+          <span>${data[home_box7CarouselNumber + i - num].year}</span>
+          <p>${data[home_box7CarouselNumber + i - num].description}</p>
+          <button class="left" onClick="home_box7CarouselSwitch(-1)"><</button>
+          <button class="right" onClick="home_box7CarouselSwitch(1)">></button>
+        `;
+      carouselItem[i].classList.add("active")
+    } else {
+      if (home_box7CarouselNumber + i - num >= data.length || home_box7CarouselNumber + i - num < 0) {
+        carouselItem[i].innerHTML = "";
+      } else {
+        carouselItem[i].innerHTML = `
+          <span>${data[home_box7CarouselNumber + i - num].year}</span>
+          <p>${data[home_box7CarouselNumber + i - num].description}</p>
+        `;
+      }
+
+    }
+    carouselBox.appendChild(carouselItem[i]);
+  }
+}
+
+home_box7Carousel()
+window.onresize = function () {
+  home_box7Carousel();
+  home_boxPosition();
+}
